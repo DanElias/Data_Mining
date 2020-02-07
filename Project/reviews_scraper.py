f72216fda8c50a36f9d790de4980082881c71188
Dan <A01208905@itesm.mx>
2020-02-07 21:02:00 +0800

    [DAN] Web scraper updated for server urls

--- Project/reviews_scraper.py
+++ Project/reviews_scraper.py
@@ -1,45 +1,64 @@
 
+'''
+    Copyright 2020 Daniel Elias Becerra
+    This web scraper is only used for educational purposes
+'''
+
 import urllib.request
 from bs4 import BeautifulSoup
 from datetime import datetime
 import json
 import re
 
-url = "https://www.yelp.com/biz/soup-nutsy-toronto?page_src=related_bizes"
+urls = [
+    "https://www.yelp.com/biz/guzman-y-gomez-singapore-5",
+    "https://www.yelp.com/biz/soup-nutsy-toronto?page_src=related_bizes",
+    "https://www.yelp.com/biz/chilis-niagara-falls-2?osq=Chillis"
+    ]
 
-request = urllib.request.Request(url)
-response = urllib.request.urlopen(request)
-html = response.read().decode('unicode_escape')
-soup = BeautifulSoup(html, "html.parser")
+restaurants = dict()
 
-reviews_content = []
-reviews_stars = []
-reviews_date_string = []
-reviews_date_datetime = []
+file_delete = open('reviews.txt', 'w') 
+file_delete.close()
 
-for div in soup.find_all(name="p", attrs={"class":["comment__373c0__3EKjH"]}):
-    review = div.find(name="span", attrs={"class": ["lemon--span__373c0__3997G"]})
-    reviews_content.append(review.getText())
+for url in urls: 
 
-for div in soup.find_all(name="div", attrs={"class":["i-stars__373c0__3UQrn"]}):
-    reviews_stars.append(div["aria-label"])
+    request = urllib.request.Request(url)
+    response = urllib.request.urlopen(request)
+    html = response.read().decode('unicode_escape')
+    soup = BeautifulSoup(html, "html.parser")
+    type(soup)
 
-for div in soup.find_all(name="span", attrs={"class":["lemon--span__373c0__3997G"]}):
-    if(len(div.getText()) > 2):
-        if(div.getText()[2] == '/' or div.getText()[1] == '/'):
-            reviews_date_string.append(div.getText())
-            reviews_date_datetime.append(datetime.strptime(div.getText(), '%m/%d/%Y'))
+    reviews_content = []
+    reviews_stars = []
+    reviews_date_string = []
+    reviews_date_datetime = []
 
-i = 0
-reviews = dict()
-while i < len(reviews_content):
-    dictionary = dict()
-    dictionary["content"] = reviews_content[i]
-    dictionary["stars"] = int(re.sub("[^0-9]", "", reviews_stars[i+1]))
-    dictionary["date"] = reviews_date_datetime[i].isoformat()
-    dictionary["date_string"] = reviews_date_string[i]
-    reviews[i] = dictionary
-    i += 1
+    restaurant_name = soup.find(name="h1", attrs={"class":["lemon--h1__373c0__2ZHSL"]}).getText()
+
+    for div in soup.find_all(name="p", attrs={"class":["comment__373c0__3EKjH"]}):
+        review = div.find(name="span", attrs={"class": ["lemon--span__373c0__3997G"]})
+        reviews_content.append(review.getText())
+
+    for div in soup.find_all(name="div", attrs={"class":["i-stars__373c0__3UQrn"]}):
+        reviews_stars.append(div["aria-label"])
+
+    for div in soup.find_all(name="span", attrs={"class":["lemon--span__373c0__3997G"]}):
+        if(len(div.getText()) > 2):
+            if(div.getText()[2] == '/' or div.getText()[1] == '/'):
+                reviews_date_string.append(div.getText())
+                reviews_date_datetime.append(datetime.strptime(div.getText(), '%m/%d/%Y'))
+
+    i = 0
+    reviews = dict()
+    while i < len(reviews_content):
+        dictionary = dict()
+        dictionary["content"] = reviews_content[i]
+        dictionary["stars"] = int(re.sub("[^0-9]", "", reviews_stars[i+1]))
+        dictionary["date"] = reviews_date_datetime[i].isoformat()
+        reviews[i] = dictionary
+        i += 1
+    restaurants[restaurant_name] = reviews
 
 with open('reviews.txt', 'a') as file:
-     file.write(json.dumps(reviews)) 
+    file.write(json.dumps(restaurants)) 
