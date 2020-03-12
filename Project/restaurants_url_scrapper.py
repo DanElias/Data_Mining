023ac990eecb5fe51cd9b07b6de4a4157a9fba59
Dan <A01208905@itesm.mx>
2020-03-12 12:38:11 +0800

    [DAN] Finish scrapper 10,000 reviews

--- Project/restaurants_url_scrapper.py
+++ Project/restaurants_url_scrapper.py
@@ -10,10 +10,10 @@ import json
 import re
 import pickle
 
-num_page = 0
+num_page = 24
 urls = list()
 
-while num_page < 1:
+while num_page < 35:
     page_url = "https://www.yelp.com/search?cflt=restaurants&find_loc=Singapore%2C+Singapore&start=" + str(num_page*30)
     urls.append(page_url)
     num_page += 1
@@ -25,22 +25,53 @@ file_delete.close()
 count = 0
 for url in urls: 
 
-    request = urllib.request.Request(url)
-    response = urllib.request.urlopen(request)
-    html = response.read().decode('unicode_escape')
-    soup = BeautifulSoup(html, "html.parser")
-    type(soup)
+    try:
+        request = urllib.request.Request(url)
+        response = urllib.request.urlopen(request)
+        html = response.read().decode('unicode_escape')
+        soup = BeautifulSoup(html, "html.parser")
+        type(soup)
+        if response.getcode() != 200:
+            continue
+    except Exception as inst:
+        continue
 
     for a in soup.find_all(name="a", attrs={"class":["link__373c0__1G70M"]}):
         href = a["href"]
         result = href.startswith('/biz')
         result2 =  "hrid=" not in href
         if(result and result2):
-            print(count)
-            print("\n")
-            print("href: %s" % a["href"])
-            count += 1
-            restaurants_urls.append(a["href"])
+                if(len(restaurants_urls) > 0 ):
+                    url = a["href"]
+                    if(restaurants_urls[len(restaurants_urls)-1] != url+"?start=100"):
+                        count += 1
+                        print(count)
+                        print("\n")
+                        print("href: %s" % a["href"])
+                        
+                        restaurants_urls.append(a["href"])
+                        restaurants_urls.append(url+"?start=20")
+                        restaurants_urls.append(url+"?start=40")
+                        restaurants_urls.append(url+"?start=60")
+                        restaurants_urls.append(url+"?start=80")
+                        restaurants_urls.append(url+"?start=100")
+                        
+                else:
+                    count += 1
+                    print(count)
+                    print("\n")
+                    print("href: %s" % a["href"])
+
+                    url = a["href"]
+                    restaurants_urls.append(a["href"])
+                    
+                    restaurants_urls.append(url+"?start=20")
+                    restaurants_urls.append(url+"?start=40")
+                    restaurants_urls.append(url+"?start=60")
+                    restaurants_urls.append(url+"?start=80")
+                    restaurants_urls.append(url+"?start=100")
+                    
+            
 
 urls_file = open("urls.txt", "wb")             
 pickle.dump(restaurants_urls, urls_file)
--- Project/reviews_scraper.py
+++ Project/reviews_scraper.py
@@ -10,6 +10,7 @@ import pandas as pd
 import json
 import re
 import pickle
+import time
 
 print("getting urls...")
 
@@ -17,51 +18,30 @@ urls_file = open("urls.txt", "rb")
 urls = pickle.load(urls_file)
 urls_file.close
 
-
-'''
-
-urls = [
-    "https://www.yelp.com/biz/guzman-y-gomez-singapore-5",
-    "https://www.yelp.com/biz/soup-nutsy-toronto?page_src=related_bizes",
-    "https://www.yelp.com/biz/chilis-niagara-falls-2?osq=Chillis",
-    "https://www.yelp.com/biz/hoshino-coffee-singapore?page_src=related_bizes",
-    "https://www.yelp.com/biz/jimmy-monkey-cafe-and-bar-singapore?page_src=related_bizes", 
-    "https://www.yelp.com/biz/the-beast-singapore",
-    "https://www.yelp.com/biz/loof-singapore",
-    "https://www.yelp.com/biz/working-title-singapore-2",
-    "https://www.yelp.com/biz/jumbo-seafood-singapore-4",
-    "https://www.yelp.com/biz/tian-tian-hainanese-chicken-rice-singapore-7",
-    "https://www.yelp.com/biz/song-fa-bak-kut-teh-singapore-11",
-    "https://www.yelp.com/biz/liao-fan-hawker-chan-singapore-3",
-    "https://www.yelp.com/biz/din-tai-fung-singapore-5",
-    "https://www.yelp.com/biz/swee-choon-tim-sum-singapore",
-    "https://www.yelp.com/biz/salt-grill-and-sky-bar-singapore",
-    "https://www.yelp.com/biz/the-naked-finn-singapore",
-    "https://www.yelp.com/biz/zhong-guo-la-mian-xiao-long-bao-singapore",
-    "https://www.yelp.com/biz/tim-ho-wan-singapore-7",
-    "https://www.yelp.com/biz/edge-singapore",
-    "https://www.yelp.com/biz/vatos-urban-tacos-singapore",
-    "https://www.yelp.com/biz/mtr-1924-singapore-2",
-    "https://www.yelp.com/biz/cut-singapore-2",
-    "https://www.yelp.com/biz/waku-ghin-singapore",
-    "https://www.yelp.com/biz/spago-by-wolfgang-puck-singapore", 
-    "https://www.yelp.com/biz/hokkaido-ramen-santouka-singapore"
-
-    ]
-'''
-
 reviews = pd. DataFrame(columns=['restaurant_name', 'content', 'stars', 'date'])
 
 file_delete = open('reviews.json', 'w') 
 file_delete.close()
 
+#urls = ["/biz/song-fa-bak-kut-teh-singapore-11"]
+
 for url in urls: 
-    print("scrapping: https://www.yelp.com"+url+"...")
-    request = urllib.request.Request("https://www.yelp.com"+url)
-    response = urllib.request.urlopen(request)
-    html = response.read().decode('unicode_escape')
-    soup = BeautifulSoup(html, "html.parser")
-    type(soup)
+
+    try:
+        print("scrapping: https://www.yelp.com"+url)
+        if(url == "/biz/pu-dong-kitchen-singapore-2"):
+            continue
+        time.sleep(10)
+        request = urllib.request.Request("https://www.yelp.com"+url)
+        response = urllib.request.urlopen(request)
+        html = response.read().decode('unicode_escape')
+        soup = BeautifulSoup(html, "html.parser")
+        type(soup)
+        if response.getcode() != 200:
+            continue
+    except Exception as inst:
+        continue
+
 
     reviews_content = []
     reviews_stars = []
@@ -69,30 +49,43 @@ for url in urls:
     reviews_date_datetime = []
 
     restaurant_name_h1 = soup.find(name="h1", attrs={"class":["lemon--h1__373c0__2ZHSL"]})
-    if(restaurant_name_h1 is None):
-        continue
-    restaurant_name = restaurant_name_h1.getText()
+    if(restaurant_name_h1 is not None):
+        restaurant_name = restaurant_name_h1.getText()
 
     for div in soup.find_all(name="p", attrs={"class":["comment__373c0__3EKjH"]}):
         review = div.find(name="span", attrs={"class": ["lemon--span__373c0__3997G"]})
         reviews_content.append(review.getText())
 
+    if(len(reviews_content) == 0):
+        continue
+
     for div in soup.find_all(name="div", attrs={"class":["i-stars__373c0__3UQrn"]}):
         reviews_stars.append(div["aria-label"])
+    
+    if(len(reviews_stars) < len(reviews_content)):
+        continue
 
     for div in soup.find_all(name="span", attrs={"class":["lemon--span__373c0__3997G"]}):
         if(len(div.getText()) > 2):
-            if(div.getText()[2] == '/' or div.getText()[1] == '/'):
-                reviews_date_string.append(div.getText())
-                reviews_date_datetime.append(datetime.strptime(div.getText(), '%m/%d/%Y'))
+            if((div.getText()[2] == '/' or div.getText()[1] == '/') and div.getText() != "14/16 Sixth Ave" and div.getText() != "80/82 Pagoda St"):
+                    try:
+                        reviews_date_datetime.append(datetime.strptime(div.getText(), '%m/%d/%Y'))
+                    except Exception as instw:
+                        continue
 
     i = 0
     while i < len(reviews_content):
         dictionary = dict()
         dictionary["restaurant_name"] = restaurant_name
+        print("restaurant name:")
+        print(dictionary["restaurant_name"])
         dictionary["content"] = reviews_content[i]
+        #print("content")
+        #print(dictionary["content"])
         if(i+1 < len(reviews_stars)):
             dictionary["stars"] = int(re.sub("[^0-9]", "", reviews_stars[i+1]))
+            print("stars")
+            print(dictionary["stars"])
         dictionary["date"] = reviews_date_datetime[i].isoformat()
         reviews = reviews.append(dictionary, ignore_index=True)
         i += 1
